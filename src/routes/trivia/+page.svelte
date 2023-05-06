f98f0cb94f030578aa619f4a10b722ecca1aac9a
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-05-06 09:55:08 -0700

    add trivia page

---
+++ src/routes/trivia/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let data;
+  </script>
+  
+  <section class="hero" id="hero">
+    <h1 class="gradient-text" data-text="Your Text Here">{data.text.title}</h1>
+    <p>
+      {data.text.description}
+    </p>
+  </section>
+  
+  <style>
+    .hero {
+      border-radius: 0.5rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+  
+    h1 {
+      font-size: 6rem;
+      font-weight: 700;
+      margin: 0;
+      padding: 0;
+      margin: auto;
+    }
+  
+  </style>
+  
