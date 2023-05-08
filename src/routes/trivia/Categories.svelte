8d181887087f47e17e9ae39377b49a5212c89a86
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-05-08 13:27:36 -0700

    improve question size

--- src/routes/trivia/Categories.svelte
+++ src/routes/trivia/Categories.svelte
@@ -2,7 +2,7 @@
   import { SelectedCategory } from "$lib/Store";
   import { onMount } from "svelte";
   import Switch from "$lib/components/Switch.svelte";
-import { formatCategoryText } from "$lib/utils";
+  import { formatCategoryText } from "$lib/utils";
 
   let showCategories = false;
   let categories: string[] = [];
@@ -23,46 +23,15 @@ import { formatCategoryText } from "$lib/utils";
     SelectedCategory.set(category);
   }
 
-
   onMount(loadCategories);
 </script>
-<style>
-  .flex-row {
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
-  }
-  .categories-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
-  }
-
-  .category {
-    padding: 10px;
-    border-radius: 5px;
-    border: 1px solid black;
-    background-color: white;
-    color: black;
-    font-size: 1.2rem;
-    cursor: pointer;
-    transition: 0.5s;
-  }
-
-  .category:hover {
-    background-color: #ccc;
-  }
-</style>
 
 <div class="flex-row">
   <p>Show Category Selector?</p>
-<Switch bind:checked={showCategories} />
+  <Switch bind:checked={showCategories} />
 </div>
 {#if showCategories}
   <div class="categories">
-    <h2>Categories</h2>
     <div class="categories-container">
       {#each categories as category}
         <button class="category" on:click={() => selectCategory(category)}>
@@ -72,3 +41,23 @@ import { formatCategoryText } from "$lib/utils";
     </div>
   </div>
 {/if}
+
+<style>
+  .flex-row {
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+  .categories-container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+  .category {
+    width: 100%;
+    cursor: pointer;
+  }
+</style>
