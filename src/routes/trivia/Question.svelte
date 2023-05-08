5e13ca2e61214da8ff549869b90f0d20ff14d90d
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-05-08 09:47:37 -0700

    handle load new after error

--- src/routes/trivia/Question.svelte
+++ src/routes/trivia/Question.svelte
@@ -36,6 +36,7 @@
   async function loadNewQuestion() {
     triviaQuestion = null;
     result = null;
+    error = false;
     try {
       // AudioPlay.set("load_loop")
       const res = await fetch("/api/trivia", {
